05891cab84fb7a833265c41b33fe24db7fd8ae39
Nagendra Babu <nagendrababu.iosdeveloper@gmail.com>
2021-09-19 20:37:27 +0530

    Initial Commit

---
+++ MyDemoApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyDemoApplication
+//
+//  Created by Syamala on 19/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
